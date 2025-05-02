2c9239e2185edecbddffca433791c89e90e318dc
= <yuda.pinhas@skai.io>
2025-05-03 01:39:50 +0300

    scripts/fix-jenkins-deploy-scripts

--- scripts/deploy_jenkins.sh
+++ scripts/deploy_jenkins.sh
@@ -6,12 +6,12 @@ ROOT_DIR=~/Documents/GDrive/SKAI/Automation/personal/helmfiles
 cd "$ROOT_DIR"
 
 echo "Creating/Updating namespace..."
-helm upgrade --install namespace ./charts/namespace \
-  -f values/netgod-play-cluster/netgod-namespace.yaml
+kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 
 echo "Deploying Jenkins to namespace '$NAMESPACE'..."
 helm upgrade --install jenkins ./charts/jenkins \
   -f ./charts/jenkins/values.yaml \
+  -f ./values/netgod-play-cluster/netgod-jenkins.yaml \
   -n $NAMESPACE
 
 echo "Jenkins deployment initiated. Run 'kubectl get pods -n $NAMESPACE' to check status."
