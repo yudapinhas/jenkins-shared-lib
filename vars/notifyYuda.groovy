bae33b22dfa3871f8efdca6e7656a6d35bee2419
NetGod <yuda.pinhas@skai.io>
2025-05-09 00:56:59 +0200

    Update notifyYuda.groovy

--- vars/notifyYuda.groovy
+++ vars/notifyYuda.groovy
@@ -1,9 +1,11 @@
 // vars/notifyYuda.groovy
 def call(String status) {
-    emailext(
+    echo "Would send email for ${status} in job ${env.JOB_NAME} #${env.BUILD_NUMBER}"
+    // Just return a map or string, don't call emailext here
+    return [
         to: 'yuda691@gmail.com',
         subject: "Build ${status}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
         body: """<p>Build <b>${status}</b>: <a href="${env.BUILD_URL}">${env.JOB_NAME} #${env.BUILD_NUMBER}</a></p>""",
         mimeType: 'text/html'
-    )
+    ]
 }
