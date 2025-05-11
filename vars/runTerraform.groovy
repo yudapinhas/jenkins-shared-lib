dcc3e71db47bf7f31583784b71b03cd607e57960
NetGod <yuda.pinhas@skai.io>
2025-05-11 18:53:42 +0200

    Update runTerraform.groovy

--- vars/runTerraform.groovy
+++ vars/runTerraform.groovy
@@ -1,5 +1,7 @@
 // vars/runTerraform.groovy
 def call(String action = 'plan') {
-    sh "terraform init"
-    sh "terraform ${action}" -var-file="dev.tfvars"
+    sh """
+      terraform init
+      terraform ${action} -var-file=dev.tfvars
+    """
 }
